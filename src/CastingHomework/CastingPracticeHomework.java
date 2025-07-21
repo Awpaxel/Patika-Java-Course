d84089524143a2f8e9c99567b9ab966e462af606
Bora Süt (Awpaxel) <bora.sut@gmail.com>
2025-07-21 16:56:37 +0300

    CastingPracticeHomework

---
+++ src/CastingHomework/CastingPracticeHomework.java
@@ -0,0 +1,33 @@
+package CastingHomework;
+
+import java.util.Scanner;
+
+public class CastingPracticeHomework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Kullanıcıdan tam sayı al
+        System.out.println("Tam sayı giriniz: ");
+        int intNumber = scanner.nextInt();
+
+        //Kullanıcıdan ondalıklı sayı al
+        System.out.println("Ondalıklı sayı giriniz:");
+        double doubleNumber = scanner.nextDouble();
+
+        //Dönüştürme
+        double intToDouble = (double) intNumber;
+
+        int doubleToInt = (int) doubleNumber;
+
+
+        //Sonuçların yazdırılması
+        System.out.println("Girdiğiniz tam sayı: " + intNumber);
+        System.out.println("Girdiğiniz tam sayının ondalıklı hali: " + intToDouble);
+
+        System.out.println("Girdiğiniz ondalıklı sayı: " + doubleNumber);
+        System.out.println("Girdiğiniz ondalıklı sayının tam sayı hali: " + doubleToInt);
+
+        scanner.close();
+
+    }
+}
